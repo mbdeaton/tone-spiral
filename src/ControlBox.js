a116fcee6743a863ce3ee22538122261b65ce2dc
Brett Deaton <brett.deaton@gmail.com>
2023-04-20 21:50:38 -0700

    Add more ui to ControlBox

--- src/ControlBox.js
+++ src/ControlBox.js
@@ -1,13 +1,52 @@
 import React from "react";
 import Box from "@mui/joy/Box";
-import PlayButton from "./PlayButton";
+import Button from "@mui/joy/Button";
+import Radio from "@mui/joy/Radio";
+import RadioGroup from "@mui/joy/RadioGroup";
+
 import "./ControlBox.css";
 
 function ControlBox() {
   return (
     <Box className="control-box" sx={{ display: "flex", gap: 4 }}>
       control box
-      <PlayButton />
+      <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
+        <RadioGroup>
+          {["equal", "just", "free"].map((item) => (
+            <Radio
+              key={item}
+              value={item}
+              disableIcon
+              label={item}
+              variant="soft"
+              color="neutral"
+              sx={{
+                px: 2,
+                alignItems: "center",
+              }}
+              slotProps={{
+                action: ({ checked }) => ({
+                  sx: {
+                    ...(checked && {
+                      bgcolor: "background.surface",
+                      boxShadow: "md",
+                      "&:hover": {
+                        bgcolor: "background.surface",
+                      },
+                    }),
+                  },
+                }),
+              }}
+            />
+          ))}
+        </RadioGroup>
+      </Box>
+      <Button variant="soft" color="primary">
+        play
+      </Button>
+      <Button variant="soft" color="neutral">
+        names
+      </Button>
     </Box>
   );
 }
